19c5d829649058148ceb10b667f6cbe55c392787
u19192372 <102986611+u19192372@users.noreply.github.com>
2023-10-23 22:48:16 +0200

    updated weather icons

--- src/script.js
+++ src/script.js
@@ -48,8 +48,13 @@ function showTemp(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
-  tempElement.innerHTML = `${temp} °C`;
+  tempElement.innerHTML = `${temp} `;
   description.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
